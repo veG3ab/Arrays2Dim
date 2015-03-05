227a510ec5ae5c2669f4ad2bc8b9be0ef2e7030f
lsu <lovro.suhadolnik@gmail.com>
2015-03-05 21:33:48 +0100

    Initial commit

---
+++ Arrays2Dim.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * Knjižnica,
+ * ki omogoča delo z
+ * dovdimenzionalnimi tabelami
+ *
+ */
+public class Arrays2Dim{
+
+
+    public static void main(String... args){
+
+        System.out.println("Test");
+
+    }
+    
+
+}
